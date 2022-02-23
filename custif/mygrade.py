8f80aa20924aeb8c2df47511dcfe03e2ec3dc018
ghanekma <ghanekma@users.noreply.github.com>
2022-02-23 14:54:33 +0000

    writing custom code

--- custif/mygrade.py
+++ custif/mygrade.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
-"""Alta3 Research | RZFeeser
-   if, elif, else - A simple program using conditionals to make a decision."""
+""" Mandar's Custome program
+
+    if, elif, else - A simple program using conditionals to make a decision."""
 
 message = 'P.S. '
 
 # wrap connection in a float() to accept decimals as numbers
-numscore = float(input("What is your numeric score between 1 to 100?"))
+# Below 3 lines are customized by the instructor
+numscore = -1
+while numscore < 0: 
+    numscore = float(input("Enter your score between 1 to 100: "))
 
 
-#if numscore > 100:
-   # message = message + 'Please enter a numeric score less than 100.'
+if numscore > 100:
+    message = message + 'Please enter a numeric score less than 100.'
 #if numscore <= 100:
    # message = message + 'Your letter grade associatd with this number is A.'
-
 # if input value was higher or equal to 90
-if numscore >= 90:
+elif numscore >= 90:
     message = message + 'Your letter grade associated with this number is A.'
 elif numscore >= 80:
     message = message + 'Your letter grade associated with this number is B.'
